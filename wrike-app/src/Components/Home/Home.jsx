8dd98546a9e8b2c2f49fc83589526dbf717a996f
Aarif shazZ <85449424+AariF-ShazZ@users.noreply.github.com>
2022-11-10 21:56:39 +0530

    day_3 commit

--- wrike-app/src/Components/Home/Home.jsx
+++ wrike-app/src/Components/Home/Home.jsx
@@ -1,12 +1,56 @@
-import { Text } from '@chakra-ui/react'
+import { Text, Box, Image, Heading, Stack, Input,WrapItem,Button, Grid, SimpleGrid, Flex } from '@chakra-ui/react'
 import React from 'react'
-
-const Home = () => {
+import { companyLogo, dataImage } from './data'
+import {HiArrowSmRight} from "react-icons/hi"
+ const Home = () => {
   return (
+    <SimpleGrid>
+    <SimpleGrid minChildWidth="100px" spacingX={5} spacingY={10} bg="#181f38" w="100%" h="700px" m="0px" p="0px" boxSizing="border-box">
+      <Box  w="90%" h="700px" m="auto" display="flex" alignItems="center" justifyContent="center">
+        <Box  ml="5%" w="75%" h="500px" textAlign="left">
+         <Heading mb="3%"  color="#ffffff" fontSize="50px">Remove barriers,</Heading>
+         <Heading mb="3%"  color="#ffffff" fontSize="50px">find clarity,</Heading>
+         <Heading mb="3%"  color="#ffffff" fontSize="50px">exceed goals</Heading>
+          <Text mb="3%"  fontSize="22px" color="#ffffff">Anything is possible with the most powerful work management software at your fingertips.</Text>
+          <Box mt="10%" h={"20px"} w="100%" display="flex" alignItems="center" justifyContent="space-between">
+            <Stack spacing={3}  w="100%" >
+              <Input bg="white" borderRadius="10px" w="100%" p="28px" pl="20px" fontSize="18px" focusBorderColor='lime' placeholder='Enter your business email' />   
+            </Stack>
+            <Button fontSize="20px" bg="#08CF65" colorScheme='green' w="43%" ml="2%" h="100%" p="5% 7%" color="#ffffff">Get Started</Button>
+          </Box>
+        </Box >
 
-    <Text as='b' fontSize={'50px'}>
-      Home
-    </Text>
+        <Box w="90%" h="500px">
+          <Image w="98%" h="570px" ml="10%" mt="5%" src='https://web-static.wrike.com/cdn-cgi/image/format=auto,quality=80,width=672,dpr=1/tp/storage/uploads/95fbafc9-6609-4b2d-aa0b-ceba4f4c4cbb/rebrand-hero-illustration-homepage-screenshots-dark-mobile.png' alt='homeImage' />
+        </Box>
+      </Box>
+       
+    </SimpleGrid>
+    <SimpleGrid bg="#EBF5F3"  w={"100%"} h="auto" pb="5%">
+      <Box p={"5%"} w="85%" m="auto" display={"flex"} alignItems={"center"} justifyContent={"space-between"} column={7} >
+        {
+          companyLogo.map((img) => <Box key={img.id}><Image   src={img.img}></Image></Box>)
+        }
+      </Box>
+      <Text mt="-40px" fontFamily= "'Anek Odia', sans-serif" fontSize={"18px"} fontWeight="400" color={"gray"} >TRUSTED BY 20,000+ ORGANIZATIONS WORLDWIDE</Text>
+     <Box w="70%" m="auto" mt="5%" mb="5%">
+      <Heading fontSize={"45px"}>Transform the way your teams work</Heading>
+      <Text mt="3%" fontSize={"25px"} color={"gray"} lineHeight={"30px"}>Empower your teams to do the best work of their lives with 360° visibility, true cross-departmental collaboration, and powerful automation.</Text>
+     </Box>
+    <Box  m="auto" w="80%" display={"flex"} alignItems={"center"} justifyContent={'space-between'}>
+      {
+        dataImage.map((ele) => <Box  h="auto" bg="#ffffff"  p='25px' m="1%" key={ele.id}><Image src={ele.img}/>
+          <Heading  fontSize={"25px"}  textAlign={"left"} mt="7%">{ele.title}</Heading>
+          <Text fontWeight={400} lineHeight={"30px"} textAlign={"left"} mt="4%" fontSize={"20px"}>{ele.des}</Text>
+          <Box color={"#0a615a"}   m="8% 0"  pl="-10px" display={"flex"} alignItems={"center"} justifyContent={"center"}>
+          <Text pl="-0.5" fontSize={"20px"} ml="-48%">Learn More</Text><HiArrowSmRight/>
+          </Box>
+        </Box>)
+      }
+    </Box>
+    </SimpleGrid>
+    </SimpleGrid>
+   
   )
 }
 
---
+++ wrike-app/src/Components/Home/data.jsx
@@ -0,0 +1,58 @@
+export const companyLogo = [
+    {
+        id: 1,
+        img: "https://web-static.wrike.com/tp/storage/uploads/3b19367a-9020-408b-8af7-5e89773b8d4b/1440-logoroll-logo-google.svg"
+    },
+    {
+        id: 2,
+        img: "https://web-static.wrike.com/tp/storage/uploads/17e81db2-0802-47a7-8037-8cb11d166299/1440-logoroll-logo-syneos.svg"
+    },
+    {
+        id: 3,
+        img: "https://web-static.wrike.com/tp/storage/uploads/3b19367a-9020-408b-8af7-5e89773b8d4b/1440-logoroll-logo-lyft.svg"
+    },
+    {
+        id: 4,
+        img: "https://web-static.wrike.com/tp/storage/uploads/1440-logoroll-logo-siemens.svg"
+    },
+    {
+        id: 5,
+        img: "https://web-static.wrike.com/tp/storage/uploads/473c0f49-59e8-4422-89d7-184c44f362cf/1440-logoroll-logo-nickelodeon.svg"
+    },
+    {
+        id: 6,
+        img: "https://web-static.wrike.com/tp/storage/uploads/3b19367a-9020-408b-8af7-5e89773b8d4b/1440-logoroll-logo-oglivy.svg"
+    },
+    {
+        id: 70,
+        img: "https://web-static.wrike.com/tp/storage/uploads/1440-logoroll-logo-snowflake.svg"
+    },
+]
+
+
+export const dataImage = [
+    {
+        id:1,
+        img:"https://web-static.wrike.com/cdn-cgi/image/format=auto,quality=80,width=224,dpr=1/tp/storage/uploads/f12bf685-64af-4413-a42e-80e58ed088ee/evo-icp-hp-ps-light.png",
+        title:"Professional Services",
+        des:"Take service delivery to the next level with Wrike’s powerful platform. Manage resources, track billable hours, and make informed decisions with ease."
+    },
+    {
+        id:2,
+        img:"https://web-static.wrike.com/cdn-cgi/image/format=auto,quality=80,width=224,dpr=1/tp/storage/uploads/8b283fef-6bd1-4513-8398-228095b64833/evo-icp-hp-marketing-light.png",
+        title:"Marketing",
+        des:"Empower your marketing team with customizable workflows, leading DAM tools, and dynamic reporting to help create high-performing campaigns."
+    },
+    {
+        id:3,
+        img:"https://web-static.wrike.com/cdn-cgi/image/format=auto,quality=80,width=224,dpr=1/tp/storage/uploads/4486fa19-a0c3-4f16-8d12-60ddfefec7c8/evo-icp-hp-pm-light.png",
+        title:"Project Management",
+        des:"Be the collaborative force that unifies your business with improved planning, risk management, and automation software to deliver better results."
+    },
+    {
+        id:4,
+        img:"https://web-static.wrike.com/cdn-cgi/image/format=auto,quality=80,width=224,dpr=1/tp/storage/uploads/0eadfaae-5c62-44fe-a375-cfdaab468ac0/evo-icp-hp-it-light.png",
+        title:"IT",
+        des:"Meet workforce and delivery needs by optimizing agile IT workflows. Manage strategic initiatives and service requests alike in the same place."
+    },
+]
